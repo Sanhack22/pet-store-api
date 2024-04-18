4f59bdc4c8fb78b0a213975225722f271f167481
Santiago <penasantiago2030@gamilcom>
2024-04-18 13:55:44 -0500

    Estructura de carpetas inicial, creacion de servidor y configuracion de envs

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hola Mundo')
+import {Server} from '../src/presentacion/server'
+import { envs } from "./config"; // importar el archivo envs de config
+(()=> {
+    main();
+  })()
+  
+  async function main (){
+  // todo: await base de datos
+  
+  // todo: inicio de nuestro server
+  new Server({
+    port: envs.PORT, // asignamos la variable de entorno en nuestra instancia de servidor.
+  }).start();
+  }

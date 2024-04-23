2ae3dac0776aa8b7e7eddab08d1dcc440abcc8b0
Santiago <penasantiago2030@gamilcom>
2024-04-23 13:33:48 -0500

    Creacion de Rutas de la Api

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import {Server} from '../src/presentacion/server'
+import {Server} from './presentation/server'
 import { envs } from "./config"; // importar el archivo envs de config
+import { AppRoutes } from './presentation/router';
 (()=> {
     main();
   })()
@@ -10,5 +11,6 @@ import { envs } from "./config"; // importar el archivo envs de config
   // todo: inicio de nuestro server
   new Server({
     port: envs.PORT, // asignamos la variable de entorno en nuestra instancia de servidor.
+    routes:AppRoutes.routes
   }).start();
   }

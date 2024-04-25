ec2060f7baaca02aa75075ee2b661d01aea63497
Santiago <penasantiago2030@gamilcom>
2024-04-25 13:16:43 -0500

    Creacion de controller del modulo auth de la Api

--- src/presentation/auth/router.ts
+++ src/presentation/auth/router.ts
@@ -1,21 +1,15 @@
 import { Router } from 'express'; // 2
+import { AuthController } from './controller'; // 1
 
 export class AuthRoutes { // 1
 
     static get routes(): Router { //3
   
       const router = Router(); //4 
-  
+      const controller = new AuthController(); // 2
         // Definir todas mis rutas especificas
-        router.post('/login', (req, res) => {  //6 
-            res.json({message: 'login'})
-        })
-      
-        router.post('/register', (req, res) => { //7 
-            res.json({message: 'register'})
-        })
-  
-  
+        router.post('/login', controller.loginUser) // 3
+        router.post('/register', controller.registerUser) //4
         return router; // 5
     }
   

f1e68636aeee34a34e11acf54a5ba22a871f3149
Santiago <penasantiago2030@gamilcom>
2024-05-14 16:32:56 -0500

    conectar base de datos mongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
-import {Server} from './presentation/server'
-import { envs } from "./config"; // importar el archivo envs de config
-import { AppRoutes } from './presentation/router';
-(()=> {
-    main();
-  })()
-  
-  async function main (){
+import { envs } from "./config";
+import { MongoDatabase } from "./data/mongodb"; //2
+import { AppRoutes } from "./presentation/router"; //1
+import { Server } from "./presentation/server";
+
+(() => {
+  main();
+})();
+
+async function main() {
   // todo: await base de datos
-  
+  await MongoDatabase.connect({//1
+    //3
+    dbName: envs.MONGO_DB_NAME,
+    mongoUrl: envs.MONGO_URL,
+  });
+
   // todo: inicio de nuestro server
   new Server({
-    port: envs.PORT, // asignamos la variable de entorno en nuestra instancia de servidor.
-    routes:AppRoutes.routes
+    port: envs.PORT,
+    routes: AppRoutes.routes, //2
   }).start();
-  }
+}

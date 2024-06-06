d990a92f1fab58ab789bc762474c647b1c53d442
Santiago Peña <penasantiago2030gmail.com>
2024-06-06 15:33:48 -0500

    creacion de usuario en base de datos mongoDB

--- src/infraestructure/datasources/auth.datasource.impl.ts
+++ src/infraestructure/datasources/auth.datasource.impl.ts
@@ -1,25 +1,49 @@
-import { AuthDataSource, CustomError, RegisterUserDto, UserEntity } from "../../domain"; //2
+import { UserModel } from "../../data/mongodb/models/users.model"; //9
+import {
+  AuthDataSource,
+  CustomError,
+  RegisterUserDto,
+  UserEntity,
+} from "../../domain"; //2
 
-export class AuthDataSourceImpl implements AuthDataSource {//1
-    async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {//3
-        const {name, email, password} = registerUserDto;// 4
+export class AuthDataSourceImpl implements AuthDataSource {
+  //1
+  async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
+    //3
+    const { name, email, password } = registerUserDto; // 4
 
-        try{//5
+    try {
+      //5
 
+      // verficar si el correo existe
+      const exists = await UserModel.findOne({ email });
+      if (exists) throw CustomError.badRequest("User already exists");
 
-            return new UserEntity(// 6
-                '1', 
-                name, 
-                email, 
-                password, 
-                ['USER']);    
+      // Crear el usuario
+      const user = await UserModel.create({
+        name: name,
+        email: email,
+        password: password,
+      });
+      // Guardamos el usuario en la base de datos
+      await user.save();
 
-        }catch(error){
-            if(error instanceof CustomError){//7
-                throw error;
-            }
-            throw CustomError.internalServer();//8
-        }
+      // TODO: Hash de contraseña
+
+      // TODO: Mapear la respuesta a nuestra entidad
+      return new UserEntity( // 6
+        user.id, // se reemplazó '1'
+        name,
+        email,
+        password,
+        user.roles //se reemplazó ['USER']
+      );
+    } catch (error) {
+      if (error instanceof CustomError) {
+        //7
+        throw error;
+      }
+      throw CustomError.internalServer(); //8
     }
-    
+  }
 }
